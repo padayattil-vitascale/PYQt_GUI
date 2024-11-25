82c5566d4b9666b33ce93ee8ed7840be46fd1ced
padayattil-vitascale <padayattil@vitascale.de>
2024-11-25 12:43:18 +0100

    Filtered responce with filtred windspeed

--- src/Calc_PPM.py
+++ src/Calc_PPM.py
@@ -12,75 +12,75 @@ def predict_ppm_on_distance(df):
         expo = 0
 
         if row['Distance'] == 200:
-            if row['Windspeed_max'] <= 0.74:
-                const = 10.984 * math.log(row['Windspeed_max']) + 30.714
-                expo = -0.3 * math.log(row['Windspeed_max']) + 2.235
-            elif 0.74 < row['Windspeed_max'] <= 0.96:
-                const = 22.24 * math.log(row['Windspeed_max']) + 39.48
-                expo = -0.241 * math.log(row['Windspeed_max']) + 2.255
+            if row['Windspeed_max_filtered'] <= 0.74:
+                const = 10.984 * math.log(row['Windspeed_max_filtered']) + 30.714
+                expo = -0.3 * math.log(row['Windspeed_max_filtered']) + 2.235
+            elif 0.74 < row['Windspeed_max_filtered'] <= 0.96:
+                const = 22.24 * math.log(row['Windspeed_max_filtered']) + 39.48
+                expo = -0.241 * math.log(row['Windspeed_max_filtered']) + 2.255
             else:
-                const = 18.456 * math.log(row['Windspeed_max']) + 36.805
-                expo = -0.0186 * math.log(row['Windspeed_max']) + 2.355
+                const = 18.456 * math.log(row['Windspeed_max_filtered']) + 36.805
+                expo = -0.0186 * math.log(row['Windspeed_max_filtered']) + 2.355
         elif row['Distance'] == 250:
-            if row['Windspeed_max'] <= 0.65:
-                const = 10.135 * math.log(row['Windspeed_max']) + 34.925
-                expo = -0.191 * math.log(row['Windspeed_max']) + 2.305
-            elif 0.65 < row['Windspeed_max'] <= 0.86:
-                const = 21.14 * math.log(row['Windspeed_max']) + 45.52
-                expo = -0.215 * math.log(row['Windspeed_max']) + 2.26
+            if row['Windspeed_max_filtered'] <= 0.65:
+                const = 10.135 * math.log(row['Windspeed_max_filtered']) + 34.925
+                expo = -0.191 * math.log(row['Windspeed_max_filtered']) + 2.305
+            elif 0.65 < row['Windspeed_max_filtered'] <= 0.86:
+                const = 21.14 * math.log(row['Windspeed_max_filtered']) + 45.52
+                expo = -0.215 * math.log(row['Windspeed_max_filtered']) + 2.26
             else:
-                const = 32.598 * math.log(row['Windspeed_max']) + 49.8
-                expo = -0.326 * math.log(row['Windspeed_max']) + 2.274
+                const = 32.598 * math.log(row['Windspeed_max_filtered']) + 49.8
+                expo = -0.326 * math.log(row['Windspeed_max_filtered']) + 2.274
         elif row['Distance'] == 300:
-            if row['Windspeed_max'] <= 0.55:
-                const = 7.272 * math.log(row['Windspeed_max']) + 36.55
-                expo = -0.046 * math.log(row['Windspeed_max']) + 2.415
-            elif 0.55 < row['Windspeed_max'] <= 0.77:
-                const = 17.03 * math.log(row['Windspeed_max']) + 51.3
-                expo = -0.012 * math.log(row['Windspeed_max']) + 2.35
+            if row['Windspeed_max_filtered'] <= 0.55:
+                const = 7.272 * math.log(row['Windspeed_max_filtered']) + 36.55
+                expo = -0.046 * math.log(row['Windspeed_max_filtered']) + 2.415
+            elif 0.55 < row['Windspeed_max_filtered'] <= 0.77:
+                const = 17.03 * math.log(row['Windspeed_max_filtered']) + 51.3
+                expo = -0.012 * math.log(row['Windspeed_max_filtered']) + 2.35
             else:
-                const = 44.86 * math.log(row['Windspeed_max']) + 62.57
-                expo = -0.519 * math.log(row['Windspeed_max']) + 2.125
+                const = 44.86 * math.log(row['Windspeed_max_filtered']) + 62.57
+                expo = -0.519 * math.log(row['Windspeed_max_filtered']) + 2.125
         elif row['Distance'] == 350:
-            if row['Windspeed_max'] <= 0.5:
-                const = 9.696 * math.log(row['Windspeed_max']) + 48.344
-                expo = -0.127 * math.log(row['Windspeed_max']) + 2.18
-            elif 0.5 < row['Windspeed_max'] <= 0.74:
-                const = 26.78 * math.log(row['Windspeed_max']) + 72.24
-                expo = -0.213 * math.log(row['Windspeed_max']) + 2.02
+            if row['Windspeed_max_filtered'] <= 0.5:
+                const = 9.696 * math.log(row['Windspeed_max_filtered']) + 48.344
+                expo = -0.127 * math.log(row['Windspeed_max_filtered']) + 2.18
+            elif 0.5 < row['Windspeed_max_filtered'] <= 0.74:
+                const = 26.78 * math.log(row['Windspeed_max_filtered']) + 72.24
+                expo = -0.213 * math.log(row['Windspeed_max_filtered']) + 2.02
             else:
-                const = 22.085 * math.log(row['Windspeed_max']) + 63.9
-                expo = 0.0542 * math.log(row['Windspeed_max']) + 2.3
+                const = 22.085 * math.log(row['Windspeed_max_filtered']) + 63.9
+                expo = 0.0542 * math.log(row['Windspeed_max_filtered']) + 2.3
         elif row['Distance'] == 400:
-            if row['Windspeed_max'] <= 0.43:
-                const = 10.035 * math.log(row['Windspeed_max']) + 57.529
-                expo = -0.047 * math.log(row['Windspeed_max']) + 2.29
-            elif 0.43 < row['Windspeed_max'] <= 0.61:
-                const = 26.62 * math.log(row['Windspeed_max']) + 81
-                expo = -0.187 * math.log(row['Windspeed_max']) + 2.09
+            if row['Windspeed_max_filtered'] <= 0.43:
+                const = 10.035 * math.log(row['Windspeed_max_filtered']) + 57.529
+                expo = -0.047 * math.log(row['Windspeed_max_filtered']) + 2.29
+            elif 0.43 < row['Windspeed_max_filtered'] <= 0.61:
+                const = 26.62 * math.log(row['Windspeed_max_filtered']) + 81
+                expo = -0.187 * math.log(row['Windspeed_max_filtered']) + 2.09
             else:
-                const = 29.94 * math.log(row['Windspeed_max']) + 81.5
-                expo = -0.07 * math.log(row['Windspeed_max']) + 2.2
+                const = 29.94 * math.log(row['Windspeed_max_filtered']) + 81.5
+                expo = -0.07 * math.log(row['Windspeed_max_filtered']) + 2.2
         elif row['Distance'] == 450:
-            if row['Windspeed_max'] <= 0.37:
-                const = 4.27 * math.log(row['Windspeed_max']) + 48.39
-                expo = 0.176 * math.log(row['Windspeed_max']) + 2.8
-            elif 0.37 < row['Windspeed_max'] <= 0.51:
-                const = 24.43 * math.log(row['Windspeed_max']) + 81.18
-                expo = -0.08 * math.log(row['Windspeed_max']) + 2.31
+            if row['Windspeed_max_filtered'] <= 0.37:
+                const = 4.27 * math.log(row['Windspeed_max_filtered']) + 48.39
+                expo = 0.176 * math.log(row['Windspeed_max_filtered']) + 2.8
+            elif 0.37 < row['Windspeed_max_filtered'] <= 0.51:
+                const = 24.43 * math.log(row['Windspeed_max_filtered']) + 81.18
+                expo = -0.08 * math.log(row['Windspeed_max_filtered']) + 2.31
             else:
-                const = 47.351 * math.log(row['Windspeed_max']) + 103
-                expo = -0.457 * math.log(row['Windspeed_max']) + 2.03
+                const = 47.351 * math.log(row['Windspeed_max_filtered']) + 103
+                expo = -0.457 * math.log(row['Windspeed_max_filtered']) + 2.03
         elif row['Distance'] == 500:
-            if row['Windspeed_max'] <= 0.32:
-                const = 5.268 * math.log(row['Windspeed_max']) + 60.087
-                expo = 0.1185 * math.log(row['Windspeed_max']) + 2.65
-            elif 0.32 < row['Windspeed_max'] <= 0.51:
-                const = 9.84 * math.log(row['Windspeed_max']) + 73.55
-                expo = 0.156 * math.log(row['Windspeed_max']) + 2.54
+            if row['Windspeed_max_filtered'] <= 0.32:
+                const = 5.268 * math.log(row['Windspeed_max_filtered']) + 60.087
+                expo = 0.1185 * math.log(row['Windspeed_max_filtered']) + 2.65
+            elif 0.32 < row['Windspeed_max_filtered'] <= 0.51:
+                const = 9.84 * math.log(row['Windspeed_max_filtered']) + 73.55
+                expo = 0.156 * math.log(row['Windspeed_max_filtered']) + 2.54
             else:
-                const = 45 * math.log(row['Windspeed_max']) + 105.6
-                expo = -0.405 * math.log(row['Windspeed_max']) + 2.12
+                const = 45 * math.log(row['Windspeed_max_filtered']) + 105.6
+                expo = -0.405 * math.log(row['Windspeed_max_filtered']) + 2.12
         else:
             const = 1.0
             expo = 1.0
@@ -95,75 +95,75 @@ def predict_ppm_on_distance(df):
         expo = 0
 
         if row['Distance'] == 200:
-            if row['Windspeed_max'] <= 0.74:
-                const = (10.984 * math.log(row['Windspeed_max']) + 30.714)/4
-                expo = -0.3 * math.log(row['Windspeed_max']) + 2.235
-            elif 0.74 < row['Windspeed_max'] <= 0.96:
-                const = (22.24 * math.log(row['Windspeed_max']) + 39.48)/3.2
-                expo = -0.241 * math.log(row['Windspeed_max']) + 2.255
+            if row['Windspeed_max_filtered'] <= 0.74:
+                const = (10.984 * math.log(row['Windspeed_max_filtered']) + 30.714)/4
+                expo = -0.3 * math.log(row['Windspeed_max_filtered']) + 2.235
+            elif 0.74 < row['Windspeed_max_filtered'] <= 0.96:
+                const = (22.24 * math.log(row['Windspeed_max_filtered']) + 39.48)/3.2
+                expo = -0.241 * math.log(row['Windspeed_max_filtered']) + 2.255
             else:
-                const = (18.456 * math.log(row['Windspeed_max']) + 36.805)/3.6
-                expo = -0.0186 * math.log(row['Windspeed_max']) + 2.355
+                const = (18.456 * math.log(row['Windspeed_max_filtered']) + 36.805)/3.6
+                expo = -0.0186 * math.log(row['Windspeed_max_filtered']) + 2.355
         elif row['Distance'] == 250:
-            if row['Windspeed_max'] <= 0.65:
-                const = (10.135 * math.log(row['Windspeed_max']) + 34.925)/4
-                expo = -0.191 * math.log(row['Windspeed_max']) + 2.305
-            elif 0.65 < row['Windspeed_max'] <= 0.86:
-                const = (21.14 * math.log(row['Windspeed_max']) + 45.52)/3.2
-                expo = -0.215 * math.log(row['Windspeed_max']) + 2.26
+            if row['Windspeed_max_filtered'] <= 0.65:
+                const = (10.135 * math.log(row['Windspeed_max_filtered']) + 34.925)/4
+                expo = -0.191 * math.log(row['Windspeed_max_filtered']) + 2.305
+            elif 0.65 < row['Windspeed_max_filtered'] <= 0.86:
+                const = (21.14 * math.log(row['Windspeed_max_filtered']) + 45.52)/3.2
+                expo = -0.215 * math.log(row['Windspeed_max_filtered']) + 2.26
             else:
-                const = (32.598 * math.log(row['Windspeed_max']) + 49.8)/3.6
-                expo = -0.326 * math.log(row['Windspeed_max']) + 2.274
+                const = (32.598 * math.log(row['Windspeed_max_filtered']) + 49.8)/3.6
+                expo = -0.326 * math.log(row['Windspeed_max_filtered']) + 2.274
         elif row['Distance'] == 300:
-            if row['Windspeed_max'] <= 0.55:
-                const = (7.272 * math.log(row['Windspeed_max']) + 36.55)/3.5
-                expo = -0.046 * math.log(row['Windspeed_max']) + 2.415
-            elif 0.55 < row['Windspeed_max'] <= 0.77:
-                const = (17.03 * math.log(row['Windspeed_max']) + 51.3)/3.1
-                expo = -0.012 * math.log(row['Windspeed_max']) + 2.35
+            if row['Windspeed_max_filtered'] <= 0.55:
+                const = (7.272 * math.log(row['Windspeed_max_filtered']) + 36.55)/3.5
+                expo = -0.046 * math.log(row['Windspeed_max_filtered']) + 2.415
+            elif 0.55 < row['Windspeed_max_filtered'] <= 0.77:
+                const = (17.03 * math.log(row['Windspeed_max_filtered']) + 51.3)/3.1
+                expo = -0.012 * math.log(row['Windspeed_max_filtered']) + 2.35
             else:
-                const = (44.86 * math.log(row['Windspeed_max']) + 62.57)/3.3
-                expo = -0.519 * math.log(row['Windspeed_max']) + 2.125
+                const = (44.86 * math.log(row['Windspeed_max_filtered']) + 62.57)/3.3
+                expo = -0.519 * math.log(row['Windspeed_max_filtered']) + 2.125
         elif row['Distance'] == 350:
-            if row['Windspeed_max'] <= 0.5:
-                const = (9.696 * math.log(row['Windspeed_max']) + 48.344)/3.5
-                expo = -0.127 * math.log(row['Windspeed_max']) + 2.18
-            elif 0.5 < row['Windspeed_max'] <= 0.74:
-                const = (26.78 * math.log(row['Windspeed_max']) + 72.24)/3.1
-                expo = -0.213 * math.log(row['Windspeed_max']) + 2.02
+            if row['Windspeed_max_filtered'] <= 0.5:
+                const = (9.696 * math.log(row['Windspeed_max_filtered']) + 48.344)/3.5
+                expo = -0.127 * math.log(row['Windspeed_max_filtered']) + 2.18
+            elif 0.5 < row['Windspeed_max_filtered'] <= 0.74:
+                const = (26.78 * math.log(row['Windspeed_max_filtered']) + 72.24)/3.1
+                expo = -0.213 * math.log(row['Windspeed_max_filtered']) + 2.02
             else:
-                const = (22.085 * math.log(row['Windspeed_max']) + 63.9)/3.3
-                expo = 0.0542 * math.log(row['Windspeed_max']) + 2.3
+                const = (22.085 * math.log(row['Windspeed_max_filtered']) + 63.9)/3.3
+                expo = 0.0542 * math.log(row['Windspeed_max_filtered']) + 2.3
         elif row['Distance'] == 400:
-            if row['Windspeed_max'] <= 0.43:
-                const = (10.035 * math.log(row['Windspeed_max']) + 57.529)/2.9
-                expo = -0.047 * math.log(row['Windspeed_max']) + 2.29
-            elif 0.43 < row['Windspeed_max'] <= 0.61:
-                const = (26.62 * math.log(row['Windspeed_max']) + 81)/2.6
-                expo = -0.187 * math.log(row['Windspeed_max']) + 2.09
+            if row['Windspeed_max_filtered'] <= 0.43:
+                const = (10.035 * math.log(row['Windspeed_max_filtered']) + 57.529)/2.9
+                expo = -0.047 * math.log(row['Windspeed_max_filtered']) + 2.29
+            elif 0.43 < row['Windspeed_max_filtered'] <= 0.61:
+                const = (26.62 * math.log(row['Windspeed_max_filtered']) + 81)/2.6
+                expo = -0.187 * math.log(row['Windspeed_max_filtered']) + 2.09
             else:
-                const = (29.94 * math.log(row['Windspeed_max']) + 81.5)/3
-                expo = -0.07 * math.log(row['Windspeed_max']) + 2.2
+                const = (29.94 * math.log(row['Windspeed_max_filtered']) + 81.5)/3
+                expo = -0.07 * math.log(row['Windspeed_max_filtered']) + 2.2
         elif row['Distance'] == 450:
-            if row['Windspeed_max'] <= 0.37:
-                const = (4.27 * math.log(row['Windspeed_max']) + 48.39)/2.9
-                expo = 0.176 * math.log(row['Windspeed_max']) + 2.8
-            elif 0.37 < row['Windspeed_max'] <= 0.51:
-                const = (24.43 * math.log(row['Windspeed_max']) + 81.18)/2.6
-                expo = -0.08 * math.log(row['Windspeed_max']) + 2.31
+            if row['Windspeed_max_filtered'] <= 0.37:
+                const = (4.27 * math.log(row['Windspeed_max_filtered']) + 48.39)/2.9
+                expo = 0.176 * math.log(row['Windspeed_max_filtered']) + 2.8
+            elif 0.37 < row['Windspeed_max_filtered'] <= 0.51:
+                const = (24.43 * math.log(row['Windspeed_max_filtered']) + 81.18)/2.6
+                expo = -0.08 * math.log(row['Windspeed_max_filtered']) + 2.31
             else:
-                const = (47.351 * math.log(row['Windspeed_max']) + 103)/3
-                expo = -0.457 * math.log(row['Windspeed_max']) + 2.03
+                const = (47.351 * math.log(row['Windspeed_max_filtered']) + 103)/3
+                expo = -0.457 * math.log(row['Windspeed_max_filtered']) + 2.03
         elif row['Distance'] == 500:
-            if row['Windspeed_max'] <= 0.32:
-                const = (5.268 * math.log(row['Windspeed_max']) + 60.087)/2.3
-                expo = 0.1185 * math.log(row['Windspeed_max']) + 2.65
-            elif 0.32 < row['Windspeed_max'] <= 0.51:
-                const = (9.84 * math.log(row['Windspeed_max']) + 73.55)/2.3
-                expo = 0.156 * math.log(row['Windspeed_max']) + 2.54
+            if row['Windspeed_max_filtered'] <= 0.32:
+                const = (5.268 * math.log(row['Windspeed_max_filtered']) + 60.087)/2.3
+                expo = 0.1185 * math.log(row['Windspeed_max_filtered']) + 2.65
+            elif 0.32 < row['Windspeed_max_filtered'] <= 0.51:
+                const = (9.84 * math.log(row['Windspeed_max_filtered']) + 73.55)/2.3
+                expo = 0.156 * math.log(row['Windspeed_max_filtered']) + 2.54
             else:
-                const = (45 * math.log(row['Windspeed_max']) + 105.6)/2.3
-                expo = -0.405 * math.log(row['Windspeed_max']) + 2.12
+                const = (45 * math.log(row['Windspeed_max_filtered']) + 105.6)/2.3
+                expo = -0.405 * math.log(row['Windspeed_max_filtered']) + 2.12
         else:
             const = 1.0
             expo = 1.0
